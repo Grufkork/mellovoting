b50937bc48768d64f1bbe48f05a2ba6f5ed5fc56
grufkork <griffin.hiscoke@me.com>
2018-03-27 16:53:25 +0000

    Added the code that disappeared

--- CodeToTXT.js
+++ CodeToTXT.js
@@ -1,3 +1,12 @@
 const fs = require("fs");
 
-fs.readFileSync()
+var codes = JSON.parse(fs.readFileSync("data/codes.json")).codes;
+
+var empty = "";
+for (var i = 0; i < codes.length; i++){
+    
+        empty += codes[i][0] + " ";
+}
+fs.writeFileSync("data/codes.txt", empty);
+
+process.exit(0);
